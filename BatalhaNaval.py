f4c19143f9cbbc05d7f1d96bef5d8f5e4e4fef3b
JoaoLuiz <joaoluiz.si7@gmail.com>
2016-09-01 20:11:10 -0300

    Primeira versão batalha naval monolitico

--- BatalhaNaval.py
+++ BatalhaNaval.py
@@ -1,20 +1,36 @@
+import base64
+import random
 class BatalhaNaval(object):
 
 	AGUA = 0
 	NAVIO = 1
 	NAUFRAGIO = 'X'
+	MAX_JOGADAS = 10
+	NOME_DO_ARQUIVO = 'jogo_batalha_naval.txt'
 
-	def __init__(self, w, h):
-		self.tabuleiro = [[self.AGUA for x in range(w)] for y in range(h)] 
+	# def __init__(self, w, h):
+	# 	self.tabuleiro = [[self.AGUA for x in range(w)] for y in range(h)] 
 
-	def insereNavio(self, x, y):
-		self.tabuleiro[x][y] = self.NAVIO
+	def insere_navio(self, x, y):
+			self.tabuleiro[x][y] = self.NAVIO
+			self.MAX_JOGADAS -= 1
 
-	def destroiNavio(self, x, y):
-		if self.tabuleiro[x][y] == self.NAVIO:
-			self.tabuleiro[x][y] = self.NAUFRAGIO
+	def destroi_navio(self, x, y):
+		if self.MAX_JOGADAS > 0:
+			if self.tabuleiro[x][y] == self.NAVIO:
+				self.tabuleiro[x][y] = self.NAUFRAGIO
+				print("---------------------------------------------------------\n\n")
+				print("^.................. Navio naufragado ..................^")
+				print("\n\n---------------------------------------------------------")
+			else:
+				print("---------------------------------------------------------\n\n")
+				print("^.................. Errrrôôôooo ..................^")
+				print("\n\n---------------------------------------------------------")
+			self.MAX_JOGADAS -= 1
+		else:
+			print("JOGADAS EXCEDIDAS")
 
-	def exibeTabuleiro(self):
+	def exibe_tabuleiro(self):
 		for i in range(len(self.tabuleiro)):
 			linha = "|"
 			for j in range(len(self.tabuleiro[i])):
@@ -26,6 +42,73 @@ class BatalhaNaval(object):
 					linha = linha + " " + "NAUFRAGIO" + " |"
 			print(linha)
 
-	def exibeTabuleiroSimples(self):
+	def exibe_tabuleiro_simples(self):
 		for i in range(len(self.tabuleiro)):
 			print(self.tabuleiro[i])
+
+	def salva_estado_do_jogo(self):
+		print("Salvando")
+
+	def to_json(self):
+		json = {
+			'AGUA': self.AGUA,
+			'NAVIO': self.NAVIO,
+			'NAUFRAGIO': self.NAUFRAGIO,
+			'MAX_JOGADAS': self.MAX_JOGADAS,
+			'tabuleiro': self.tabuleiro
+		}
+		return json
+
+	def salvar(self):
+		json_string = str(self.to_json())
+		self.escreve_arquivo(json_string)
+
+	def escreve_arquivo(self, string):
+		arquivo = open(BatalhaNaval.NOME_DO_ARQUIVO,'w+')
+		arquivo.write(str(self.criptografa(string))) 
+		arquivo.close()
+	
+	def criptografa(self, string):
+		return base64.b64encode(bytes(string, "utf-8"))
+
+	@staticmethod
+	def json_to_object(json):
+		classe = BatalhaNaval()
+		classe.AGUA = json.get('AGUA')
+		classe.NAVIO = json.get('NAVIO')
+		classe.NAUFRAGIO = json.get('NAUFRAGIO')
+		classe.MAX_JOGADAS = json.get('MAX_JOGADAS')
+		classe.tabuleiro = json.get('tabuleiro')
+		return classe
+
+	@staticmethod
+	def carregar_jogo():
+		string = BatalhaNaval.ler_arquivo()
+		json = eval(string)
+		return BatalhaNaval.json_to_object(json)
+
+	@staticmethod
+	def ler_arquivo():
+		arquivo = open(BatalhaNaval.NOME_DO_ARQUIVO,'r')
+		string = arquivo.read()
+		arquivo.close()
+		if len(string) > 0:
+			return BatalhaNaval.descriptografa(string)
+		else:
+			jogo = BatalhaNaval.json_to_object(eval("{'NAUFRAGIO': 'X', 'MAX_JOGADAS': 10, 'AGUA': 0, 'NAVIO': 1, 'tabuleiro': []}"))
+			jogo.tabuleiro = BatalhaNaval.gera_tabuleiro_randomico()
+			return str(jogo.to_json())
+
+	@staticmethod
+	def gera_tabuleiro_randomico():
+		tabuleiro = [[0 for x in range(10)] for y in range(10)] 
+		for i in range(50):
+			x = random.randint(0, 9)
+			y = random.randint(0, 9)
+			tabuleiro[x][y] = 1
+		return tabuleiro
+
+	@staticmethod
+	def descriptografa(string):
+		str_hash = string[2:-1]
+		return base64.b64decode(str_hash).decode("utf-8", "ignore")
--- main.py
+++ main.py
@@ -1,8 +1,55 @@
 from BatalhaNaval import BatalhaNaval
+import base64
+# jogo = BatalhaNaval(5,5)
+# jogo.insere_navio(1,1)
+# jogo.insere_navio(4,1)
+# jogo.insere_navio(2,3)
+# jogo.destroi_navio(2,3)
 
-jogo = BatalhaNaval(5,5)
-jogo.insereNavio(1,1)
-jogo.insereNavio(4,1)
-jogo.insereNavio(2,3)
-jogo.destroiNavio(2,3)
-jogo.exibeTabuleiroSimples()
+jogo = BatalhaNaval.carregar_jogo()
+
+def mostra_menu():
+	print("---------- BATALHA NAVAL ----------")
+	print("'0' para sair\n")
+	print("'1' para inserir navio\n")
+	print("'2' para destruir navio\n")
+	print("'3' para exibir o tabuleiro\n")
+	print("'4' para salvar o jogo\n")
+
+def executa_comando(comando):
+	if comando == 0:
+		return 0
+	elif comando == 1:
+		x = int(input('Insira a posição X para inserir navio: '))
+		y = int(input('Insira a posição y para inserir navio: '))
+		jogo.insere_navio(x, y)
+		return 1
+	elif comando == 2:
+		x = int(input('Insira a posição X para destruir navio: '))
+		y = int(input('Insira a posição y para destruir navio: '))
+		jogo.destroi_navio(x, y)
+		return 2
+	elif comando == 3:
+		jogo.exibe_tabuleiro_simples()
+		return 3
+	elif comando == 4:
+		jogo.salvar()
+		return 4
+
+def inicia_jogo():
+	jogando = 1
+	while jogando != 0:
+		mostra_menu()		
+		comando = input('Insira um comando: ')
+		jogando = executa_comando(int(comando))
+
+	print("FIM DO JOGO")
+		
+inicia_jogo()
+# jogo.salvar()
+# string = "b'eydOQVVGUkFHSU8nOiAnWCcsICdNQVhfSk9HQURBUyc6IC00LCAnQUdVQSc6IDAsICdOQVZJTyc6IDEsICd0YWJ1bGVpcm8nOiBbWzAsIDAsIDAsIDAsIDBdLCBbMCwgMSwgMCwgMCwgMF0sIFswLCAwLCAwLCAnWCcsIDBdLCBbMCwgMCwgMCwgMCwgMF0sIFswLCAxLCAwLCAwLCAwXV19'"
+
+# a = string[2:-1] #base64.b64encode(bytes(string, "utf-8"))
+# b = base64.b64decode(a)
+
+# print(b)
