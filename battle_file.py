f6628b971d79ceed5a08bd6cd29a96ec66f78746
edilson <fcoedilson@gmail.com>
2016-09-08 05:59:38 -0300

    arquivo de log

---
+++ battle_file.py
@@ -0,0 +1,41 @@
+import os
+import json
+
+
+class Info:
+    file = "battleship.json"
+    dir = "db"
+    def __init__(self):
+        pass
+
+class Database(object):
+    def __init__(self):
+        self.file = os.path.join(Info.dir, Info.file)
+        self.validate()
+
+    def validate(self):
+        if not os.path.exists(Info.dir):
+            os.makedirs(Info.dir)
+        if not os.path.isfile(self.file):
+            open(self.file, 'x').close()
+
+    def has_saved(self):
+        return os.path.isfile(self.file) and os.stat(self.file).st_size > 0
+
+    def get_saved(self):
+        with open(self.file, 'r') as file:
+            return json.loads(file.read())
+
+    def restart(self, db):
+        self.remove()
+        self.validate()
+        self.save(db)
+
+    def save(self, db):
+        with open(self.file, 'w') as outfile:
+            json.dump(db, outfile, indent=4, sort_keys=False, separators=(',', ':'))
+
+    def remove(self):
+	    if self.has_saved_game():
+	        os.remove(os.path.join(Info.dir, Info.file))
+	        os.rmdir(Info.dir)
---
+++ console.py
@@ -0,0 +1,9 @@
+import os
+
+class Console(object):
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def clear():
+        os.system('clear')
