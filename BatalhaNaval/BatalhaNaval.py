83ea987addb44a4cece7e563a14725757fc66d19
andre damasio de queiroz paiva <1411800>
2016-09-22 19:27:46 -0300

    Upload files

--- BatalhaNaval/BatalhaNaval.py
+++ BatalhaNaval/BatalhaNaval.py
@@ -1,24 +1,39 @@
+import random
+
+
 class BatalhaNaval(object):
 
 	def __init__(self, numeroDeJogadas):
 		self.matriz = [["~" for j in range(5)] for i in range(5)]
 		self.numeroDeJogadas = numeroDeJogadas
-		
+		if self.existeJogo():
+			self.carregaPosicoesBarcos()
+			self.carregaAcertos()
+			self.carregaNumeroJogadas()
+		else:
+			self.initBarcos()
 
 	def initBarcos(self):
-		self.matriz[0][1]="@"
-		self.matriz[3][2]="@"
-		self.matriz[2][4]="@"
-		self.salvaLogDoTabuleiro(self.matriz)
+		for i in range(3):
+			eixoX = random.randint(0, 4)
+			eixoY = random.randint(0, 4)
+			if self.matriz[eixoX][eixoY] == "@":
+				i -= 1
+			else:
+				self.matriz[eixoX][eixoY] = "@"
+				self.salvaPosicoesDosBarcos(eixoX, eixoY)
+			self.salvaLogDoTabuleiro(self.matriz)
 
-	def salvaLogDeJogada(self, eixoX, eixoY):
-		self.arq = open("LogDeJogadas.txt", "a")
-		self.arq.write("X:"+str(eixoX)+"\n")
-		self.arq.write("Y:"+str(eixoY)+"\n")
+# Salvar-----------------------------------------------------------
+
+	def salvaLogDeAcerto(self, eixoX, eixoY):
+		self.arq = open("LogDeAcertos.txt", "a")
+		self.arq.write(str(eixoX) + "-" + str(eixoY) + "\n")
 		self.arq.close()
 
-	def limpaLog(self, log):
-		self.arq = open(log, "w")
+	def salvaPosicoesDosBarcos(self, eixoX, eixoY):
+		self.arq = open("LogPosicaoBarcos.txt", "a")
+		self.arq.write(str(eixoX) + "-" + str(eixoY) + "\n")
 		self.arq.close()
 
 	def salvaLogDoTabuleiro(self, matriz):
@@ -27,21 +42,64 @@ class BatalhaNaval(object):
 		self.arq.close()
 
 	def salvaLogNumJogadas(self, numeroDeJogadas):
-		self.arq = open("LogNumeroDeJogadas.txt", "w")
-		self.arq.write(str(numeroDeJogadas))
+		self.arq = open("LogNumeroDeJogadas.txt", "a")
+		self.arq.write(str(numeroDeJogadas)+"\n")
 		self.arq.close()
 
+# Jogada-----------------------------------------------------------------------------------
+
 	def jogada(self):
-		self.numeroDeJogadas -= 1
-		self.salvaLogNumJogadas(self.numeroDeJogadas)
+		print("Jogadas disponiveis: " + str(self.numeroDeJogadas))
 		eixoX = int(input("Insira a coordenada do eixo X: "))
 		eixoY = int(input("Insira a coordenada do eixo Y: "))
-		self.salvaLogDeJogada(eixoX, eixoY)
 		if self.matriz[eixoX][eixoY] == "@":
 			self.matriz[eixoX][eixoY] = "*"
 			self.salvaLogDoTabuleiro(self.matriz)
+			self.salvaLogDeAcerto(eixoX, eixoY)
+			self.decrementaJogada()
 			print("Acertou o barco!")
 		elif self.matriz[eixoX][eixoY] == "~":
+			self.decrementaJogada()
 			print("Acertou a agua!")
 		else:
-			print("Barco ja afundado.")
+			print("Barco ja afundado. \n Tente novamente.")
+
+	def decrementaJogada(self):
+		self.numeroDeJogadas -= 1
+		self.salvaLogNumJogadas(self.numeroDeJogadas)
+
+# Carregar----------------------------------------------------------------------------
+
+	def existeJogo(self):
+		arq = open("LogDoTabuleiro.txt", "r")
+		if arq.readline() == "":
+			existe = False
+		else:
+			existe = True
+		return existe
+
+	def carregaPosicoesBarcos(self):
+		arq = open("LogPosicaoBarcos.txt", "r")
+		for linha in arq:
+			valor = linha.split("-")
+			self.matriz[int(valor[0])][int(valor[1])] = "@"
+		arq.close()
+
+	def carregaAcertos(self):
+		arq = open("LogDeAcertos.txt", "r")
+		for linha in arq:
+			valor = linha.split("-")
+			self.matriz[int(valor[0])][int(valor[1])] = "*"
+		arq.close()
+
+	def carregaNumeroJogadas(self):
+		arq = open("LogNumeroDeJogadas.txt", "r")
+		for linha in arq:
+			self.numeroDeJogadas = int(linha)
+		arq.close()
+
+# Limpar------------------------------------------------------------------------------------------
+
+	def limpaLog(self, log):
+		self.arq = open(log, "w")
+		self.arq.close()
--- BatalhaNaval/executaBatalha.py
+++ BatalhaNaval/executaBatalha.py
@@ -1,11 +1,13 @@
 import BatalhaNaval
 
 a = BatalhaNaval.BatalhaNaval(3)
-a.initBarcos()
+# a.initBarcos()
 while a.numeroDeJogadas > 0:
 	a.jogada()
-	print("Jogadas disponiveis: " + str(a.numeroDeJogadas))
 print("Acabaram as jogadas")
-# a.limpaLog("LogDeJogadas.txt")
-# a.limpaLog("LogDoTabuleiro.txt")
-# a.limpaLog("LogNumeroDeJogadas.txt")
+#Metodo sendo usado assim para facilitar testes.
+# TODO corrigir metodo
+a.limpaLog("LogDeAcertos.txt")
+a.limpaLog("LogDoTabuleiro.txt")
+a.limpaLog("LogNumeroDeJogadas.txt")
+a.limpaLog("LogPosicaoBarcos.txt")
