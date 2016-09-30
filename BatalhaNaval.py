e33ea7f644d7e3636d8b5b9e68ea3c7b22f43949
Marcos Saraiva <msaraiva@gmail.com>
2016-09-29 22:28:03 -0300

    Modificado para modelo Cliente-Servidor. Nesse caso, o cliente recebe um tabuleiro gerado pelo servidor.

--- BatalhaNaval.py
+++ BatalhaNaval.py
@@ -2,13 +2,16 @@ import os
 import random
 import pickle
 import pandas as pd
+import socket
+import sys
+import struct
+import time
 from prettytable import PrettyTable
 
 class BatalhaNaval(object):
   
-  VERSAO = "0.2"
   SAVEFILE = "savefile.dat"
-  
+  VERSAO = "0.2"
   linhas = 5
   colunas = 5
   
@@ -21,17 +24,68 @@ class BatalhaNaval(object):
 
   pecas = ["0", "1", "X"] # 0 - Água; 1 - Barco; X - Atingido
   
-  def converteColuna(self, col):
-    if (col.lower() == 'a'):
-      return 0
-    elif (col.lower() == 'b'):
-      return 1
-    elif (col.lower() == 'c'):
-      return 2
-    elif (col.lower() == 'd'):
-      return 3
-    elif (col.lower() == 'e'):
-      return 4
+  def conectaServidor(self, sock, host, port):
+    # Connect the socket to the port where the server is listening
+    server_address = (host, port)
+    print('connecting to {0} port {1}'.format(host, port), file=sys.stderr)
+    sock.connect(server_address)
+    
+  def desconectaServidor(self, sock):
+    sock.close()
+    
+  def enviaComando(self, sock, cmd, data):
+    data = pickle.dumps(dict(cmd=cmd, data=data))
+    print(cmd)
+    sock.sendall(data)
+    
+  def recebeComando(self, sock):
+    #make socket non blocking
+#    sock.setblocking(0)
+    connData = sock.recv(8192)
+    
+    try:
+      data = pickle.loads(connData)
+    except EOFError:
+      return {}
+    
+    return data
+    
+#  def recebeComando(self, sock, timeout):
+#      #make socket non blocking
+#      sock.setblocking(0)
+#       
+#      #total data partwise in an array
+#      total_data=bytes()
+#      #data='';
+#       
+#      #beginning time
+#      begin=time.time()
+#      while 1:
+#          #if you got some data, then break after timeout
+#          if total_data and time.time()-begin > timeout:
+#              break
+#           
+#          #if you got no data at all, wait a little longer, twice the timeout
+#          elif time.time()-begin > timeout*2:
+#              break
+#           
+#          #recv something
+#          try:
+#              data = pickle.loads(sock.recv(8192))
+#              print(data)
+#              if data:
+#                  total_data = data
+#                  #change the beginning time for measurement
+#                  begin = time.time()
+#              else:
+#                  #sleep for sometime to indicate a gap
+#                  time.sleep(0.1)
+#          except:
+#              pass
+#       
+#      #join all parts to make final string
+#      print(total_data)
+#      return total_data
  
   def iniciaTabuleiro(self):
     barcosInseridos = 0
@@ -44,111 +98,13 @@ class BatalhaNaval(object):
           if (barcosInseridos > self.maxBarcos):
             x = 0
         self.tabuleiro[i].append(self.pecas[x])
+  
+  def resetaTabuleiro(self):
+    self.tabuleiro = [[]]
     
   def imprimeTabuleiro(self):
     print("-=-=-=-=-=-=-=-=-=-=-=-=\tTabuleiro\t-=-=-=-=-=-=-=-=-=-=-=-=\n\n")
-    print(pd.DataFrame(self.tabuleiro, columns=['A', 'B', 'C', 'D', 'E']))
-    self.pressioneQualquer()
-  
-  def jogar(self):
-    perdeu = 0
-    
-    while (self.jogadasRestantes > 0):
-      self.limpaTela()
-      
-      print("\t\t\t\t\tBarcos atingidos: " + str(self.numBarcosAtingidos))
-      print("\t\t\t\t\tJogadas restantes: " + str(self.jogadasRestantes))
-      print("\t\t\t\t\tJogadas efetuadas:" + str(self.jogadasEfetuadas) + "\n\n")
-      col = input("Digite a coluna que sera atingida(a, b, c, d ou e): ")
-      pos = col
-      col = self.converteColuna(col)
-      lin = int(input("Digite a linha que sera atingida(0, 1, 2, 3 ou 4): "))
-      pos = pos + str(lin)
-          
-      try:
-        peca = self.tabuleiro[lin][col]
-      except:
-        print("Posicao invalida!")
-        self.pressioneQualquer()
-        continue
-      
-      self.jogadasEfetuadas = self.jogadasEfetuadas + " " + pos
-      print("\n\nAtirando na posicao " + pos + "!\n")
-      
-      if (peca == 'X'):
-        print("Ops, ja destruimos esse barco...")
-      elif (int(peca) == 0):
-        print("Ahh, aqui tem apenas agua...")
-        self.jogadasEfetuadas = self.jogadasEfetuadas + "(O)"
-      elif (int(peca) == 1):
-        print("Atingimos um barco!")
-        self.numBarcosAtingidos = self.numBarcosAtingidos + 1
-        self.tabuleiro[lin][col] = 'X'
-        self.jogadasEfetuadas = self.jogadasEfetuadas + "(X)"
-        if (self.numBarcosAtingidos == 0):
-          print("Destruimos todos os barcos, voce venceu!\n\n")
-          break
-      
-      self.jogadasRestantes = self.jogadasRestantes - 1
-      
-      if (self.jogadasRestantes == 0):
-        perdeu = 1
-        print("Nao restam mais jogadas, voce perdeu!\n\n")
-        self.pressioneQualquer()
-        break
-      
-      op = input("\nDeseja continuar o jogo(s/n)? ")
-      if (op.lower() == 's'):
-        continue
-      elif (op.lower() == 'n'):
-        op = input("Salvar o jogo(s/n)? ")
-        if (op.lower() == 's'):
-          self.salvaJogo()
-          break
-        elif (op.lower() == 'n'):
-          perdeu = 1
-          break
-    
-    if ((perdeu == 1) or (self.numBarcosAtingidos == 0)):
-      self.imprimeTabuleiro()
-    
-    self.mostraMenu()  
-
-  def limpaTela(self):
-    if (os.name == 'nt'):
-      _=os.system("cls")
-    else:
-      _=os.system("clear")
-    
-  def mostraMenu(self):
-    self.limpaTela()
-    print("Batalha Naval - " + self.VERSAO + "\n\n")
-    print("1 - Novo Jogo")
-    print("2 - Continuar Jogo Anterior")
-    print("9 - Sair")
-    option = int(input("\nEscolha uma opcao: "))
-    if (option == 1):
-      self.tabuleiro = [[]]
-      self.iniciaTabuleiro()
-      self.jogar()
-    elif (option == 2):
-      if (os.path.exists(self.SAVEFILE)):
-        self.restauraJogo()
-      else:
-        print("Arquivo savefile.dat nao encontrado, iniciando novo jogo!")
-        self.pressioneQualquer()
-        self.tabuleiro = [[]]
-        self.iniciaTabuleiro()
-      self.jogar()
-    elif (option == 9):
-      print("Bye!\n")
-      quit()
-    else:
-      print("Opcao invalida!\n")
-      self.mostraMenu()
-
-  def pressioneQualquer(self):
-    input("\n\nPressione qualquer tecla para continuar...")
+    print((pd.DataFrame(self.tabuleiro, columns=['A', 'B', 'C', 'D', 'E']))) 
    
   def restauraJogo(self):
     with open(self.SAVEFILE, 'rb') as f:
@@ -162,4 +118,3 @@ class BatalhaNaval(object):
     with open(self.SAVEFILE, 'wb') as f:
       pickle.dump(dict(numBarcosAtingidos=self.numBarcosAtingidos, jogadasEfetuadas=self.jogadasEfetuadas, jogadasRestantes=self.jogadasRestantes, tabuleiro=self.tabuleiro), f)
       print("Jogo salvo!")
-      self.pressioneQualquer()
--- jogo.py
+++ jogo.py
@@ -1,6 +1,134 @@
+import os
+import random
+import pickle
+import socket
+import sys
 from BatalhaNaval import BatalhaNaval
 
-jogo = BatalhaNaval()
-#jogo.iniciaTabuleiro()
-#jogo.imprimeTabuleiro()
-jogo.mostraMenu()
+host = 'localhost'
+port = 17543
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+batalhaClient = BatalhaNaval()
+
+def converteColuna(col):
+  if (col.lower() == 'a'):
+    return 0
+  elif (col.lower() == 'b'):
+    return 1
+  elif (col.lower() == 'c'):
+    return 2
+  elif (col.lower() == 'd'):
+    return 3
+  elif (col.lower() == 'e'):
+    return 4
+  
+def jogar():
+  perdeu = 0
+  
+  while (batalhaClient.jogadasRestantes > 0):
+    limpaTela()
+    
+    print(("\t\t\t\t\tBarcos atingidos: " + str(batalhaClient.numBarcosAtingidos)))
+    print(("\t\t\t\t\tJogadas restantes: " + str(batalhaClient.jogadasRestantes)))
+    print(("\t\t\t\t\tJogadas efetuadas:" + str(batalhaClient.jogadasEfetuadas) + "\n\n"))
+    col = input("Digite a coluna que sera atingida(a, b, c, d ou e): ")
+    pos = col
+    col = converteColuna(col)
+    lin = int(input("Digite a linha que sera atingida(0, 1, 2, 3 ou 4): "))
+    pos = pos + str(lin)
+        
+    try:
+      peca = batalhaClient.tabuleiro[lin][col]
+    except:
+      print("Posicao invalida!")
+      pressioneQualquer()
+      continue
+    
+    batalhaClient.jogadasEfetuadas = batalhaClient.jogadasEfetuadas + " " + pos
+    print(("\n\nAtirando na posicao " + pos + "!\n"))
+    
+    if (peca == 'X'):
+      print("Ops, ja destruimos esse barco...")
+    elif (int(peca) == 0):
+      print("Ahh, aqui tem apenas agua...")
+      batalhaClient.jogadasEfetuadas = batalhaClient.jogadasEfetuadas + "(O)"
+    elif (int(peca) == 1):
+      print("Atingimos um barco!")
+      batalhaClient.numBarcosAtingidos = batalhaClient.numBarcosAtingidos + 1
+      batalhaClient.tabuleiro[lin][col] = 'X'
+      batalhaClient.jogadasEfetuadas = batalhaClient.jogadasEfetuadas + "(X)"
+      if (batalhaClient.numBarcosAtingidos == 0):
+        print("Destruimos todos os barcos, voce venceu!\n\n")
+        break
+    
+    batalhaClient.jogadasRestantes = batalhaClient.jogadasRestantes - 1
+    
+    if (batalhaClient.jogadasRestantes == 0):
+      perdeu = 1
+      print("Nao restam mais jogadas, voce perdeu!\n\n")
+      pressioneQualquer()
+      break
+    
+    op = input("\nDeseja continuar o jogo(s/n)? ")
+    if (op.lower() == 's'):
+      continue
+    elif (op.lower() == 'n'):
+      op = input("Salvar o jogo(s/n)? ")
+      if (op.lower() == 's'):
+        batalhaClient.salvaJogo()
+        break
+      elif (op.lower() == 'n'):
+        perdeu = 1
+        break
+  
+  if ((perdeu == 1) or (batalhaClient.numBarcosAtingidos == 0)):
+    batalhaClient.imprimeTabuleiro()
+    pressioneQualquer()
+  
+  mostraMenu()   
+  
+def limpaTela():
+  if (os.name == 'nt'):
+    _=os.system("cls")
+  else:
+    _=os.system("clear")  
+
+def mostraMenu():
+  limpaTela()
+  print(("Batalha Naval - " + batalhaClient.VERSAO + "\n\n"))
+  print("1 - Novo Jogo")
+  print("2 - Continuar Jogo Anterior")
+  print("9 - Sair")
+  option = int(input("\nEscolha uma opcao: "))
+  if (option == 1):
+    batalhaClient.resetaTabuleiro()
+    batalhaClient.enviaComando(sock, "geraTabuleiro", "")
+    dict = batalhaClient.recebeComando(sock)
+    print(dict)
+    batalhaClient.tabuleiro = dict["data"]
+    jogar()
+  elif (option == 2):
+    if (os.path.exists(batalhaClient.SAVEFILE)):
+      batalhaClient.restauraJogo()
+    else:
+      print("Arquivo savefile.dat nao encontrado, iniciando novo jogo!")
+      pressioneQualquer()
+      batalhaClient.resetaTabuleiro()
+      batalhaClient.iniciaTabuleiro()
+    jogar()
+  elif (option == 9):
+    print("Bye!\n")
+    quit()
+  else:
+    print("Opcao invalida!\n")
+    mostraMenu()    
+
+def pressioneQualquer():
+  input("\n\nPressione qualquer tecla para continuar...")
+
+#print(batalhaClient.VERSAO)
+#batalhaClient.iniciaTabuleiro()
+#batalhaClient.imprimeTabuleiro()
+batalhaClient.conectaServidor(sock, host, port)
+mostraMenu()
