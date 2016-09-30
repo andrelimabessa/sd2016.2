876f03e70f9a0da1685ab3faae320a1e2fb1cc26
Breno-Luan <brennoluan@gmail.com>
2016-09-30 00:22:47 -0300

    Add files via upload

---
+++ BatalhaNaval/Teste.py
@@ -0,0 +1,10 @@
+﻿import BatalhaNaval
+
+a = BatalhaNaval.BatalhaNaval(3)
+
+while a.numMaximoJogadas > 0:
+    a.jogada()
+    print("Você ainda possui "+str(a.numMaximoJogadas)+" jogada(s).")
+
+a.limparLogs()
+print("Fim de Jogo")
